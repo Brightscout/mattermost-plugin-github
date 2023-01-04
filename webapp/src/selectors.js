0ed74369c2fd8bea64130e02a8bb6772815229b8
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-01-04 14:53:39 +0530

    [MI-2547]:Fixed review fixes for Github issue 613 and PR 626

--- webapp/src/selectors.js
+++ webapp/src/selectors.js
@@ -1,6 +1,10 @@
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
+import {createSelector} from 'reselect';
+
 import {id as pluginId} from './manifest';
+
+const emptyArray = [];
 
 const getPluginState = (state) => state['plugins-' + pluginId] || {};
 
@@ -18,5 +22,46 @@ export const getServerRoute = (state) => {
 
     return basePath;
 };
+
+function mapPrsToDetails(prs, details) {
+    if (!prs) {
+        return [];
+    }
+
+    return prs.map((pr) => {
+        let foundDetails;
+        if (details) {
+            foundDetails = details.find((prDetails) => {
+                return (pr.repository_url === prDetails.url) && (pr.number === prDetails.number);
+            });
+        }
+        if (!foundDetails) {
+            return pr;
+        }
+
+        return {
+            ...pr,
+            status: foundDetails.status,
+            mergeable: foundDetails.mergeable,
+            requestedReviewers: foundDetails.requestedReviewers,
+            reviews: foundDetails.reviews,
+        };
+    });
+}
+
+export const getSidebarData = createSelector(
+    getPluginState,
+    (pluginState) => {
+        return {
+            username: pluginState.username,
+            reviews: mapPrsToDetails(pluginState.sidebarContent.reviews || emptyArray, pluginState.reviewDetails),
+            yourPrs: mapPrsToDetails(pluginState.sidebarContent.prs || emptyArray, pluginState.yourPrDetails),
+            yourAssignments: pluginState.sidebarContent.assignments || emptyArray,
+            unreads: pluginState.sidebarContent.unreads || emptyArray,
+            org: pluginState.organization,
+            rhsState: pluginState.rhsState,
+        };
+    },
+);
 
 export const configuration = (state) => getPluginState(state).configuration;
