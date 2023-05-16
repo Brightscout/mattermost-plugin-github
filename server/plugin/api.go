ba98e78ecabbb28478e9a729f26435d6fb50989f
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-05-16 17:16:15 +0530

    [MI-3072]:Converted review requested API to graphQL

--- server/plugin/api.go
+++ server/plugin/api.go
@@ -697,19 +697,15 @@ func (p *Plugin) getUnreadsData(c *UserContext) []*FilteredNotification {
 }
 
 func (p *Plugin) getReviewsData(c *UserContext) []*github.Issue {
-	config := p.getConfiguration()
+	graphQLClient := p.graphQLConnect(c.GHInfo)
 
-	githubClient := p.githubConnectUser(c.Context.Ctx, c.GHInfo)
-	username := c.GHInfo.GitHubUsername
-
-	query := getReviewSearchQuery(username, config.GitHubOrg)
-	result, _, err := githubClient.Search.Issues(c.Ctx, query, &github.SearchOptions{})
+	reviewResponse, err := graphQLClient.PullRequests.GetReviewRequestedPrs()
 	if err != nil {
-		c.Log.WithError(err).With(logger.LogContext{"query": query}).Warnf("Failed to search for review")
+		c.Log.WithError(err).Warnf("Failed to search for review")
 		return []*github.Issue{}
 	}
 
-	return result.Issues
+	return reviewResponse
 }
 
 func (p *Plugin) getYourPrsData(c *UserContext) []*github.Issue {
